2d1a55e8e95367bd0050d1ded7d7ed05d996349b
Tobias Anker <tobias.anker@kitsunemimi.moe>
2025-12-20 19:44:34 +0100

    docs(#779): update rest-api-docs
    Signed-off-by: Tobias Anker <tobias.anker@kitsunemimi.moe>

--- scripts/collect-api-specs.sh
+++ scripts/collect-api-specs.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-curl 127.0.0.1:11417/openapi.json | jq > ./docs/frontend/open_api_docu_miko.json
-curl 127.0.0.1:11416/openapi.json | jq > ./docs/frontend/open_api_docu_ryokan.json
-curl 127.0.0.1:11418/openapi.json | jq > ./docs/frontend/open_api_docu_hanami.json
-curl 127.0.0.1:11419/openapi.json | jq > ./docs/frontend/open_api_docu_torii.json
-curl 127.0.0.1:11421/openapi.json | jq > ./docs/frontend/open_api_docu_omamori.json
-curl 127.0.0.1:11420/openapi.json | jq > ./docs/frontend/open_api_docu_sakura.json
+curl 127.0.0.1:11417/openapi.json | jq > ./docs/user/rest_api/open_api_docu_miko.json
+curl 127.0.0.1:11416/openapi.json | jq > ./docs/user/rest_api/open_api_docu_ryokan.json
+curl 127.0.0.1:11418/openapi.json | jq > ./docs/user/rest_api/open_api_docu_hanami.json
+curl 127.0.0.1:11419/openapi.json | jq > ./docs/user/rest_api/open_api_docu_torii.json
+curl 127.0.0.1:11421/openapi.json | jq > ./docs/user/rest_api/open_api_docu_omamori.json
+curl 127.0.0.1:11420/openapi.json | jq > ./docs/user/rest_api/open_api_docu_sakura.json
