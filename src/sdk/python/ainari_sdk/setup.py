7cf4a44c1d0933eb7de22a945c76d5dcbb9f146e
Tobias Anker <tobias.anker@kitsunemimi.moe>
2026-06-08 20:10:33 +0200

    docs(#901): updates for tagging 0.11.1
    - updated Changelog and other version marker for 0.11.1
    
    Signed-off-by: Tobias Anker <tobias.anker@kitsunemimi.moe>

--- src/sdk/python/ainari_sdk/setup.py
+++ src/sdk/python/ainari_sdk/setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 # from subprocess import check_call
 import os
 
-version = os.getenv('PYTHON_PACKAGE_VERSION', '0.11.0')
+version = os.getenv('PYTHON_PACKAGE_VERSION', '0.11.1')
 
 
 # NOTE (kitsudaiki): leftover of old implementation, but left disabled
